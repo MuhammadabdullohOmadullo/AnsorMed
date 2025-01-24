b9af1809feb5fd73689f778ab97d37f522cf7f0b
MuhammadabdullohOmadullo <abdulloh2003and@gmail.com>
2025-01-24 06:05:30 +0400

    Add some changes and make site-hedor responsive

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const elSiteHeader= document.querySelector('.site-header');
+const elSiteHeaderTogglerButton = elSiteHeader.querySelector('.site-header-toggler-button');
+if (elSiteHeaderTogglerButton) {
+ elSiteHeaderTogglerButton.addEventListener ('click', function (){
+ elSiteHeader.classList.toggle('site-header-open');
+ });
+}
